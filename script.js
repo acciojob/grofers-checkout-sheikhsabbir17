5b0163fe90eab5d285744c6a9c87177f17322838
Sheikh Sabbir <144557992+sheikhsabbir17@users.noreply.github.com>
2026-09-18 20:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+ let prices = document.querySelectorAll(".price");
+  let total = 0;
+
+  prices.forEach(price => {
+    total += Number(price.innerText);
+  });
+
+  let row = document.createElement("tr");
+  let cell = document.createElement("td");
+
+  cell.innerText = total;
+  row.appendChild(cell);
+  document.querySelector("table").appendChild(row);
 };
+
+getSumBtn.addEventListener("click", getSum);
 
 getSumBtn.addEventListener("click", getSum);
 
